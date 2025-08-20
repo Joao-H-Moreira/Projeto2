9607af2d18e270195db11b1cdb7ac79fdb10481d
Joao-H-Moreira <joao.ham2001@gmail.com>
2025-08-20 19:01:50 -0300

    ja foi metade

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Receita(models.Model):
+    tittle = models.CharField(max_length=200)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    image = models.ImageField(upload_to='receitas/images/', blank=True, null=True)
