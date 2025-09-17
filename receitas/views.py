0344c34689ed0e2380f7e26f683c1ce13306301a
Joao-H-Moreira <joao.ham2001@gmail.com>
2025-09-17 19:16:07 -0300

    att html forms

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from django.core.mail import send_mail
 from .models import Receita
+from .forms import ContatoForm
 
 # receitas/views.py 
+def sobre_nos(request):
+    return render(request, 'receitas/sobre_nos.html')
+
+def contato(request):
+    if request.method == 'POST':
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            mensagem = form.cleaned_data['mensagem']
+            
+            send_mail(
+                f'Mensagem de {nome}',
+                f'Mensagem de {nome} ({email}):\n\n{mensagem}',
+                email,
+                ['seu_email_para_receber@exemplo.com'],
+                fail_silently=False
+            )
+            #redireciona para a página de sucesso
+            return redirect(reverse('sucesso'))
+    else:
+        form = ContatoForm()
+    return render(request, 'receitas/contato.html', {'form':form})
+
+def sucesso(request):
+    return render(request, 'receitas/sucesso.html')
+            
 def home(request): 
     categoria_slug = request.GET.get('categoria')
     categorias_choices = [choice[0] for choice in Receita.CATEGORIAS]
