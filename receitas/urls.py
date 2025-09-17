3d85ad04744287da93c941e94429b673ac3f6fce
Joao-H-Moreira <joao.ham2001@gmail.com>
2025-09-17 19:41:18 -0300

    att urls

--- receitas/urls.py
+++ receitas/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [ 
 path('', views.home, name='home'), 
+path('sibre_nos/', views.sobre_nos, name='sobre_nos'),
 path('receita/<int:id>/', views.receita_detail, name='receita_detail'),
 path('pesquisar/', views.pesquisar_receitas, name='pesquisar_receitas'),
+path('contato/', views.contato, name='contato' ),
+path('sucesso/', views.sucesso, name='sucesso' ),
 ] 
